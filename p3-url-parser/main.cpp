97e5bf111cdc8e0f1e1e1b3221d2ddddcf324811
Jake Short <46853220+Jake-Short@users.noreply.github.com>
2022-02-09 15:28:30 -0800

    Created URL parser

--- p3-url-parser/main.cpp
+++ p3-url-parser/main.cpp
@@ -8,7 +8,32 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Please enter a URL: ";
+    std::string input;
+    std::cin >> input;
+    
+    // Find the index of the first "/" of the "//" substring
+    int slashSlashIndex = (int)input.find("//");
+    std::string scheme = input.substr(0, slashSlashIndex);
+    
+    // Set input to remaining string starting with the "//"
+    input = input.substr(slashSlashIndex + 2, input.length());
+    
+    // Find the beginning of the path
+    int pathBeginIndex = (int)input.find("/");
+    std::string authority = "//" + input.substr(0, pathBeginIndex);
+    
+    // Set the input equal to the rest of the string (the path)
+    // and initialize path variable that is equal to the input
+    input = input.substr(pathBeginIndex, input.length());
+    std::string path = input;
+
+    // Print everything out
+    std::cout << scheme << std::endl;
+    
+    std::cout << authority << std::endl;
+    
+    std::cout << path << std::endl;
+    
     return 0;
 }
